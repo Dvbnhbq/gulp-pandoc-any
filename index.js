c2ee0c477c34b13fac07b3cde5abda6008416a0b
Legal Prime GSC, JSC <develop@legalprimegsc.ru>
2019-06-25 13:43:52 +0300

    feat(writer): Update `index.js` to write any file types

--- index.js
+++ index.js
@@ -1,47 +1,105 @@
-/* Modules */
+/*
+ * Ported from code by brightSPARK Labs
+ * www.brightsparklabs.com
+ *
+ * Andrew Pilsch updated this to work with ODT, DOCX, and other files
+ * that gulp-pandoc-pdf doesn't work with.
+ */
 
-var pdc     = require('pdc');
+'use strict';
+
+// -----------------------------------------------------------------------------
+// MODULES
+// -----------------------------------------------------------------------------
+
+var pdc     = require('pdc-any');
 var through = require('through2');
 var gutil   = require('gulp-util');
+var mkdirp  = require('mkdirp');
+var path    = require('path');
 
-/* Plugin */
+// -----------------------------------------------------------------------------
+// PLUGIN
+// -----------------------------------------------------------------------------
 
 var PluginError = gutil.PluginError;
-var PluginName  = 'gulp-pandoc';
+var pluginName  = 'gulp-pandoc-any';
 
-/* Exports */
+// -----------------------------------------------------------------------------
+// EXPORTS
+// -----------------------------------------------------------------------------
 
 module.exports = function(opts) {
-  var from = opts.from;
-  var to   = opts.to;
-  var ext  = opts.ext;
-  var args = opts.args || [];
+    var args = opts.args || [];
+    var outputDir = opts.outputDir;
+	
+	/**support for input and output file types */
+	
+	var inputFileType = opts.inputFileType || '.md'
+	//var outputFileType = opts.outputFileType || '.docx';
+	var outputFileType = opts.outputFileType;
+	var outputType;
 
-  if (!from) { throw new PluginError(PluginName, '"from" is not defined'); }
-  if (!to) { throw new PluginError(PluginName, '"to" is not defined'); }
-  if (!ext) { throw new PluginError(PluginName, '"ext" is not defined'); }
+    return through.obj(function (file, enc, cb) {
+        var input = file.contents.toString();
+        if (file.isNull())  {
+            this.push(file);
+            return cb();
+        }
 
-  return through.obj(function (file, enc, cb) {
-    var input = file.contents.toString();
-    if (file.isNull())  { 
-      this.push(file);
-      return cb();
-    }
-    
-    if (file.isStream()) {
-      this.emit('error', new PluginError(PluginName, 'Streaming not supported')); 
-      return cb();
-    }
+        if (file.isStream()) {
+            this.emit('error', new PluginError(pluginName, 'Streaming not supported'));
+            return cb();
+        }
 
-    pdc(input, from, to, args, function(err, output) {
-      if (err) {
-        this.emit('error', err.toString());
-        return cb();
-      }
-      file.contents = new Buffer(output);
-      file.path = gutil.replaceExtension(file.path, opts.ext);
-      this.push(file);
-      return cb();
-    }.bind(this));
-  });
+        // pandoc cannot output certain file types to STDOUT (such as epub, docx,
+		// and odt). Here we need to write the file to the output destination
+		// specified via the configuration object. Once these are created 
+		// we need to ensure we send something through the pipelineto gulp,
+        // so we call pandoc again and have it stream across HTML content back
+        // to the pipeline.
+
+        var outputFile = outputDir + '/' + file.relative;
+		
+		/** support for input and output filetypes  */
+		if (outputFileType) {
+			outputFile = outputFile.replace(inputFileType, outputFileType);
+		}
+		else {
+			outputFile = outputFile.replace(inputFileType, '.pdf');
+		}
+
+        // create directory for pdf
+        var oDir = path.dirname(outputFile);
+        mkdirp(oDir, function(err) {
+            if (err) {
+                this.emit('error', err.toString());
+                return cb();
+            }
+        }.bind(this));
+
+        // create output file
+        var outputArgs = args.slice();
+        outputArgs.push('--output=' + outputFile);
+		outputType = outputFileType && outputFileType.replace(/^\./,'');
+        pdc(input, 'markdown', outputType, outputArgs, function(err, output) {
+            if (err) {
+                this.emit('error', new PluginError(pluginName, err.toString()));
+                return cb();
+            }
+        }.bind(this));
+
+        // create html
+        var htmlArgs = args.slice();
+        pdc(input, 'markdown', 'html5', htmlArgs, function(err, output) {
+            if (err) {
+                this.emit('error', new PluginError(pluginName, err.toString()));
+                return cb();
+            }
+            file.contents = new Buffer(output);
+            file.path = gutil.replaceExtension(file.path, '.html');
+            this.push(file);
+            return cb();
+        }.bind(this));
+    });
 };
